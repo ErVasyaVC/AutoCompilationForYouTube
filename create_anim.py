36dd7e6acd25bb17c810250de4ee46270ea551c9
Vasya Eremin <ervasyavc@gmail.com>
2022-09-23 00:10:01 +0300

    Multi-country support

--- create_anim.py
+++ create_anim.py
@@ -6,7 +6,7 @@ from celluloid import Camera
 import json
 
 
-def SetSetting(year, i):
+def SetSetting(year):
     plt.title(year)
     plt.axis([0, 80000, 0.5, 5.5])
 
@@ -19,30 +19,47 @@ with open('config.json', 'r') as json_file:
 time_shot = round(1 / fps, 3)
 
 data = data_base.SortYears()
+print(data)
 min_year, max_year = data[0][0][0], data[0][0][-1]
-num_year = 0
-speed_year = (max_year - min_year) / (fps * seconds)
+passed_years = 0
 float_year = 0
+speed_year = (max_year - min_year) / (fps * seconds)
 speed_amount = []
-t = 0
-for i in range(fps * seconds):
-    plt.clf()
-    for a in range(len(data[0][0])):
-        if min_year + num_year == data[0][0][a] and t != data[0][0][a]:
-            yy = data[0][0][a + 1] - data[0][0][a]
-            y_cord = data[0][1][a]
-            yyy = data[0][1][a + 1] - y_cord
-            t = data[0][0][a]
 
-    num_year = round(float_year // 1)
+t = [0 for i in range(len(data))]
+speed_amount = [0 for i in range(len(data))]
+list_y_cord = [0 for i in range(len(data))]
+
+for shot in range(fps * seconds):
+    plt.clf()
+    for num_country in range(len(data)):
+        for num_year in range(len(data[num_country][0])):
+            if min_year + passed_years == data[num_country][0][num_year] and t[num_country] != data[num_country][0][num_year]:
+                yy = data[num_country][0][num_year + 1] - data[num_country][0][num_year]
+                list_y_cord[num_country] = data[num_country][1][num_year]
+                yyy = data[num_country][1][num_year + 1] - list_y_cord[num_country]
+                t[num_country] = data[num_country][0][num_year]
+                speed_amount[num_country] = yyy / (yy / speed_year)
+                break
+    # for a in range(len(data[0][0])):
+    #     if min_year + num_year == data[0][0][a] and t != data[0][0][a]:
+    #         yy = data[0][0][a + 1] - data[0][0][a]
+    #         y_cord = data[0][1][a]
+    #         yyy = data[0][1][a + 1] - y_cord
+    #         t = data[0][0][a]
+
+    passed_years = round(float_year // 1)
     float_year += speed_year
 
-    y_cord += (yyy / (yy / speed_year))
-    SetSetting(num_year + min_year, y_cord)
 
-    plt.barh(1, y_cord)
-    plt.barh(2, 40000)
-    plt.barh(3, 30000)
-    plt.barh(4, 20000)
-    plt.barh(5, 10000)
+    # list_y_cord[0] += speed_amount[0]
+    # plt.barh(1, list_y_cord[0])
+    # plt.barh(2, 1000)
+    # print(list_y_cord[0], speed_amount[0])
+    for i in range(len(speed_amount)):
+        list_y_cord[i] += speed_amount[i]
+        plt.barh(i+1, list_y_cord[i])
+    SetSetting(passed_years + min_year)
+
     plt.pause(time_shot)
+
