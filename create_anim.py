975075b5a2a7f2ee578bd975ea44249bc55dbe74
Vasya Eremin <ervasyavc@gmail.com>
2022-09-28 19:26:36 +0300

    Working sort countries

--- create_anim.py
+++ create_anim.py
@@ -1,6 +1,4 @@
-import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-import numpy as np
 import data_base
 from celluloid import Camera
 import json
@@ -8,7 +6,8 @@ import json
 
 def SetSetting(year, i):
     plt.title(label=year, loc='right', fontsize=20, fontweight='bold', pad=-200)
-    plt.axis([0, 90000, 0.5, 5.5])
+    plt.axis([0, i, 0.5, 5.5])
+
     ax = plt.subplot()
     ax.set_title(label=year, y=-1, pad=-280, fontsize=20)
     ax.invert_yaxis()
@@ -17,16 +16,23 @@ def SetSetting(year, i):
     ax.spines.top.set_visible(False)
     ax.spines.left.set_visible(False)
     ax.spines.bottom.set_visible(False)
+    # ratio = 2
+    # x_left, x_right = ax.get_xlim()
+    # y_low, y_high = ax.get_ylim()
+    # ax.set_aspect(abs((x_right - x_left) / (y_low - y_high)) * ratio)
+
 
 
 def SortDescending(array_amount):
-    return_array = []
-    sort_array = []
-    for i in array_amount:
-        sort_array.append(i)
-    sort_array.sort()
+    return_array = [0 for i in range(len(array_amount))]
+    sort_array = array_amount.copy()
+    sort_array.sort(reverse=True)
+
+    place = 0
     for i in sort_array:
-        return_array.append(array_amount.index(i))
+        return_array[array_amount.index(i)] = place
+        place += 1
+
     return return_array
 
 
@@ -57,21 +63,20 @@ for shot in range(fps * seconds):
                 last_year[num_country] = data[num_country][0][num_year]
                 speed_amount[num_country] = difference_amount / (difference_years / speed_year)
                 break
-
-    # list_x_cord = SortDescending(list_y_cord)
-    # list_x_cord = [0 for i in range(len(data))]
-    # for num_country in range(len(data)):
-    #     list_y_cord[num_country] += speed_amount[num_country]
-    #     plt.barh(list_x_cord[num_country] + 1, list_y_cord[num_country], color=colors[num_country])
-    #     plt.text(list_y_cord[num_country], list_x_cord[num_country] + 1, data_base.list_of_countries[num_country], fontsize=11)
-
-    for num_country in range(len(data)):
         list_y_cord[num_country] += speed_amount[num_country]
-        plt.barh(num_country + 1, list_y_cord[num_country], color=colors[num_country])
-        plt.text(list_y_cord[num_country], num_country + 1, data_base.list_of_countries[num_country], fontsize=11)
+
+    list_x_cord = SortDescending(list_y_cord)
+    for num_country in range(len(data)):
+        plt.barh(list_x_cord[num_country] + 1, list_y_cord[num_country], color=colors[num_country])
+        plt.text(0, list_x_cord[num_country] + 1, data_base.list_of_countries[num_country],
+                 fontsize=11)
 
     passed_years = round(float_year // 1)
     float_year += speed_year
     SetSetting(passed_years + min_year, max(list_y_cord))
 
     plt.pause(time_shot)
+
+
+#plt.savefig('filename.png', dpi=500)
+plt.show()
